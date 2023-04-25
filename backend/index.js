503267fbff1dc9f1489283c470713a22e3eae18d
Sava Polimac <savapolimac1999@gmail.com>
2023-04-25 21:36:22 +0200

    creating user route and chat engine implementation

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
+require('dotenv').config()
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
+console.log(process.env.PRIVATE_KEY)
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users',
+      {username: username, secret: username, first_name: username},
+      {
+        headers: {
+          "private-key": process.env.PRIVATE_KEY
+        }
+      }
+    )
+    return res.status(r.status).json(r.data)
+  } catch(e) {
+    return res.status(e.response.status).json(e.response.data)
+  }
 });
 
 app.listen(3001);
